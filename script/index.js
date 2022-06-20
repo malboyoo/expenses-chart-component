72f4a65199f1a2871fa462a1dea084cd25bb5af5
malboyoo <malboyoo@gmail.com>
2022-06-20 22:34:48 +0200

    first release version

--- script/index.js
+++ script/index.js
@@ -1,2 +1,25 @@
 "use strict";
-let test = 123;
+const applyDataFile = async () => {
+    const url = "../data.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    data.sort((a, b) => b.amount - a.amount);
+    //adding to cyan color tu the higher spending day
+    const higherDay = document.querySelector(`.${data[0].day} .candle`);
+    higherDay.style.background = "var(--cyan)";
+    for (let day of data) {
+        const dayNode = document.querySelector(`.${day.day}`);
+        console.log(Math.round((day.amount / data[0].amount) * 150) / 10);
+        dayNode.style.minHeight = `${Math.round((day.amount / data[0].amount) * 150) / 10 + 2.6}rem`;
+        const spanAmount = document.querySelector(`.${day.day} .pop-up span`);
+        spanAmount.textContent = `$${day.amount}`;
+        const popupNode = document.querySelector(`.${day.day} .pop-up`);
+        dayNode.addEventListener("mouseenter", () => {
+            popupNode.style.display = "flex";
+        });
+        dayNode.addEventListener("mouseleave", () => {
+            popupNode.style.display = "none";
+        });
+    }
+};
+applyDataFile();
--- script/index.ts
+++ script/index.ts
@@ -1,1 +1,34 @@
-let test: number = 123;
+//creating interface of the file object
+interface SpendingData {
+   day: string;
+   amount: number;
+}
+const applyDataFile = async () => {
+   const url: string = "../data.json";
+   const response = await fetch(url);
+   const data: SpendingData[] = await response.json();
+
+   data.sort((a, b) => b.amount - a.amount);
+   //adding to cyan color tu the higher spending day
+   const higherDay: HTMLDivElement = document.querySelector(`.${data[0].day} .candle`)!;
+   higherDay.style.background = "var(--cyan)";
+   for (let day of data) {
+      const dayNode: HTMLDivElement = document.querySelector(`.${day.day}`)!;
+
+      // candle height are caculated in % of each value according to the top value. (the +2.6rem is for the paragraph)
+      dayNode.style.height = `${Math.round((day.amount / data[0].amount) * 150) / 10 + 2.6}rem`;
+
+      const spanAmount: HTMLSpanElement = document.querySelector(`.${day.day} .pop-up span`)!;
+      spanAmount.textContent = `$${day.amount}`;
+
+      const popupNode: HTMLDivElement = document.querySelector(`.${day.day} .pop-up`)!;
+
+      dayNode.addEventListener("mouseenter", () => {
+         popupNode.style.display = "flex";
+      });
+      dayNode.addEventListener("mouseleave", () => {
+         popupNode.style.display = "none";
+      });
+   }
+};
+applyDataFile();
